c0a268c3c92703f4bb98528f2fed3bc46108c5e5
Guwel <guwel@yandex.ru>
2022-12-03 18:08:52 +0500

    add homework

---
+++ Homework/Extratask/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int [] array = {55, 5, 12, 8, 9, 100, 10};
+int n = array.Length;
+int i = 0;
+int maxberrycount = 0;
+int berrycount = 0;
+berrycount = array[n-1] + array [i] + array [i+1];
+maxberrycount = berrycount;
+i++;
+while (i < n-1 && i!= 0)
+    {
+        berrycount = array[i-1] + array [i] + array [i+1];
+        i++;
+        while (maxberrycount < berrycount)
+        {
+            maxberrycount = berrycount;
+        }
+        if (i == n-1)
+        berrycount = array[i-1] + array [i] + array [0];
+        if (maxberrycount < berrycount)
+        maxberrycount = berrycount;
+    }
+Console.Write($"Максимальное число ягод: {maxberrycount}");
+    
+
+
+
+
---
+++ Homework/Task 21/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координаты точки А в 3D пространстве: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+double za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B в 3D пространстве: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(xa-xb,2)+Math.Pow(ya-yb,2)+Math.Pow(za-zb,2));
+Console.WriteLine($"Расстояние между точками: {result}");
+
+
+
+
+
+
+
+
+
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
